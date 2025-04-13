d6d36600a71c56e25200251ccf789e49261d624f
parsa <parsafarajpoursarabi@gmail.com>
2025-04-13 15:34:29 +0330

    salam

--- src/model/Game.java
+++ src/model/Game.java
@@ -1,5 +1,8 @@
 package model;
 
+import java.util.ArrayList;
+
 public class Game {
     public DateTime dateTime;
+    private ArrayList<Player> users , loggedInUsers;
 }
--- src/model/Player.java
+++ src/model/Player.java
@@ -14,6 +14,18 @@ public class Player {
     public String username;
     private String email, password;
 
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
     public void plant() {
 
     }
