30ab8bc1859ac8ab9a362d19444288f05d6e6a3a
parsa <parsafarajpoursarabi@gmail.com>
2025-05-13 17:47:57 +0330

    Npcs enum added!

---
+++ src/model/enums/NPCs.java
@@ -0,0 +1,15 @@
+package model.enums;
+
+import model.Item;
+import model.Quest;
+
+import java.util.ArrayList;
+
+public enum NPCs {
+    ;
+    String name, job;
+    ArrayList<Item> favoriteItems;
+    ArrayList<Quest> quests;
+    ArrayList<String> dialogues;
+
+}
