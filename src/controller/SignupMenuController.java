f9d9d4bdbdd45b73f33571db8bea1dcab198f7a7
parsasq <sadeghi.parsa.2005@gmail.com>
2025-05-18 21:23:33 +0330

    daa

--- src/controller/SignupMenuController.java
+++ src/controller/SignupMenuController.java
@@ -23,6 +23,7 @@ public class SignupMenuController extends MenuController{
 
 
     public Result enterMenu(String menuName) {
+        System.out.println(menuName);
         if (menuName.equals("main")) App.enterMenu(Menu.MainMenu);
         return new Result(Map.of("message", "you should go to main menu for this command"));
     }
